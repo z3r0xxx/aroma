92ef4fd31c80edd6c98b5b97dbda501be33515a5
z3r0xxx <arseniykvasov@gmail.com>
2021-08-16 15:29:34 +0300

    Добавил отображение favicon.ico

--- page.php
+++ page.php
@@ -4,19 +4,20 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 		<title>me - Главная</title>
-		<link rel="icon" href="img/Fevicon.png" type="image/png">
+
+		<link rel="icon" href="<?php bloginfo('template_directory') ?>/img/Fevicon.png" type="image/png">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/style.css">
+
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/bootstrap/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/fontawesome/css/all.min.css">
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/themify-icons/themify-icons.css">
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/nice-select/nice-select.css">
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/owl-carousel/owl.theme.default.min.css">
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/owl-carousel/owl.carousel.min.css">
-
-		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/style.css">
 	</head>
 	<body>
-
 		
 		<?php get_header(); ?>
 
