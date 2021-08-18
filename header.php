0f7f5026586f02c6e0f14d44a710b58f32478b6b
z3r0xxx <arseniykvasov@gmail.com>
2021-08-18 22:46:24 +0300

    Небольшие изменения

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 	  	<nav class="navbar navbar-expand-lg navbar-light">
 
 			<div class="container">
-				<a class="navbar-brand logo_h" href="index.html"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt=""></a>
+				<a class="navbar-brand logo_h" href="<?php get_home_url(); ?>"><img src="<?php bloginfo('template_directory') ?>/img/logo.png" alt=""></a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="icon-bar"></span>
