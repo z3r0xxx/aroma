7a16328c1ae35550a81b53ec9494c3e6f2e5c670
z3r0xxx <arseniykvasov@gmail.com>
2021-08-25 14:52:20 +0300

    Наконец то сделал отображение меню в футере

--- footer.php
+++ footer.php
@@ -28,26 +28,11 @@
 					<div class="single-footer-widget tp_widgets">
 						<h4 class="footer_title">Полезные ссылки</h4>
 						<ul class="list">
-							<li><a href="#">Home</a></li>
-							<li><a href="#">Shop</a></li>
-							<li><a href="#">Blog</a></li>
-							<li><a href="#">Product</a></li>
-							<li><a href="#">Brand</a></li>
-							<li><a href="#">Contact</a></li>
-						</ul>
-					</div>
-				</div>
-
-				<div class="col-lg-2 col-md-6 col-sm-6">
-					<div class="single-footer-widget instafeed">
-						<h4 class="footer_title">Галерея</h4>
-						<ul class="list instafeed d-flex flex-wrap">
-							<li><img src="<?php bloginfo('template_directory') ?>/img/gallery/r1.jpg" alt=""></li>
-							<li><img src="<?php bloginfo('template_directory') ?>/img/gallery/r2.jpg" alt=""></li>
-							<li><img src="<?php bloginfo('template_directory') ?>/img/gallery/r3.jpg" alt=""></li>
-							<li><img src="<?php bloginfo('template_directory') ?>/img/gallery/r5.jpg" alt=""></li>
-							<li><img src="<?php bloginfo('template_directory') ?>/img/gallery/r7.jpg" alt=""></li>
-							<li><img src="<?php bloginfo('template_directory') ?>/img/gallery/r8.jpg" alt=""></li>
+							<li><a href="terms">Политика обработки персональных данных</a></li>
+							<li><a href="public_ofert">Публичная оферта</a></li>
+							<li><a href="FAQ">Вопросы и ответы</a></li>
+							<li><a href="shipping_and_payment">Доставка и оплата</a></li>
+							<li><a href="#">Новости</a></li>
 						</ul>
 					</div>
 				</div>
@@ -57,16 +42,15 @@
 						<h4 class="footer_title">Контакты</h4>
 						<div class="ml-40">
 							<p class="sm-head"><span class="fa fa-location-arrow"></span>Адрес</p>
-							<p>123, Main Street, Your City</p>
+							<p>г. Санкт-Петербург</p>
 							<p class="sm-head"><span class="fa fa-phone"></span>Телефон</p>
 							<p>
-								+123 456 7890 <br>
-								+123 456 7890
+								+7 960 277 74 24 <br>
+								+7 950 036 27 47
 							</p>
 							<p class="sm-head"><span class="fa fa-envelope"></span>E-mail</p>
 							<p>
-								free@infoexample.com <br>
-								www.infoexample.com
+								mefreedom@bk.ru <br>
 							</p>
 						</div>
 					</div>
