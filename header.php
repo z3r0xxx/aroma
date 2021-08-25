e8b6f81d13ca407b7d10a87698ddfcd57a462b1c
z3r0xxx <arseniykvasov@gmail.com>
2021-08-26 00:59:55 +0300

    Изменил шрифт пунктов меню

--- header.php
+++ header.php
@@ -6,6 +6,14 @@
 <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/owl-carousel/owl.theme.default.min.css">
 <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/vendors/owl-carousel/owl.carousel.min.css">
 
+<link href="https://fonts.googleapis.com/css2?family=Tenor+Sans&display=swap" rel="stylesheet">
+
+<style>
+	.header_area {
+		font-family: 'Tenor Sans';
+	}
+</style>
+
 <!--================ Start Header Menu Area =================-->
 <header class="header_area">
 	<div class="main_menu">
@@ -42,47 +50,6 @@
 							'add_a_class'	  => 'nav-link'
 						] ); 
 					?>
-
-					<!-- <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-						<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-						<li class="nav-item submenu dropdown">
-							<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-							aria-expanded="false">Shop</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="nav-link" href="category.html">Shop Category</a></li>
-								<li class="nav-item"><a class="nav-link" href="single-product.html">Product Details</a></li>
-								<li class="nav-item"><a class="nav-link" href="checkout.html">Product Checkout</a></li>
-								<li class="nav-item"><a class="nav-link" href="confirmation.html">Confirmation</a></li>
-								<li class="nav-item"><a class="nav-link" href="cart.html">Shopping Cart</a></li>
-							</ul>
-						</li>
-
-						<li class="nav-item submenu dropdown">
-							<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-							aria-expanded="false">Blog</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-								<li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
-							</ul>
-						</li>
-
-						<li class="nav-item submenu dropdown">
-							<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-							aria-expanded="false">Pages</a>
-							<ul class="dropdown-menu">
-								<li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
-								<li class="nav-item"><a class="nav-link" href="register.html">Register</a></li>
-								<li class="nav-item"><a class="nav-link" href="tracking-order.html">Tracking</a></li>
-							</ul>
-						</li>
-						<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-					</ul> -->
-
-					<!-- <ul class="nav-shop">
-					<li class="nav-item"><button><i class="ti-search"></i></button></li>
-					<li class="nav-item"><button><i class="ti-shopping-cart"></i><span class="nav-shop__circle">3</span></button> </li>
-					<li class="nav-item"><a class="button button-header" href="#">Buy Now</a></li>
-					</ul> -->
 				</div>
 			</div>
 	  	</nav>
